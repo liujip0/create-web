dd29b253d599e26606ffbd33f5fe838b95d628d5
liujip0 <liujip2020@gmail.com>
2025-08-04 02:43:12 -0400

    follow tutorial

---
+++ main.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import { init } from './utils/createProject.ts';
+
+(async () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Project name:',
+        default: 'my-web-app'
+      },
+      {
+        type: 'list',
+        name: 'projectType',
+        message: 'Select project type:',
+        choices: [
+          {
+            name: 'Fullstack',
+            value: 'fullstack'
+          },
+          {
+            name: 'Frontend Only',
+            value: 'frontend'
+          }
+        ],
+        default: 'fullstack'
+      }
+    ])
+    .then((answers) => {
+      init(answers.projectName, answers.projectType);
+    })
+    .catch(catchError);
+})();
+
+function catchError(error: any) {
+  if (error.isTtyError) {
+    console.error('Cannot render the prompt...');
+  } else {
+    console.error(error.message);
+  }
+}
